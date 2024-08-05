6ae9e4cf19d5f058598795c0235754be307f71ad
m7medalgandy <142119759+m7medalgandy@users.noreply.github.com>
2024-08-05 23:33:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 8080;
 cors_proxy.createServer({
   originWhitelist: [], // السماح بأي مصدر
   requireHeader: [], // إزالة الشرط الخاص بالرؤوس المطلوبة
-  // لا تزيل رؤوس الكوكيز
+  removeHeaders: [], // لا تزيل أي رؤوس
+  setHeaders: { // إعداد رأس set-cookie
+    'set-cookie': 'open=true; Path=/; HttpOnly'
+  }
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
